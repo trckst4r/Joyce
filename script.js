b575dc6fe91f672f6932304d6fd7650fa309097f
trckst4r <115210322+trckst4r@users.noreply.github.com>
2024-03-29 19:20:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const img = document.querySelector(".img");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+const secondQuestion = document.querySelectorAll(".question")[1];
+yesBtn.addEventListener("click", ( ) => {
+    question.innerHTML = "Thank you for accepting my invitation, see you this sunday!!";
+    secondQuestion.innerHTML = "How about you?";
+    img.src = "https://gifdb.com/images/high/animated-pusheen-thank-you-cat-zyvngsxio2nkydxo.gif";
+});
+noBtn.addEventListener("click", ( ) => {
+    const wrapperRect = wrapper.getBoundingClientRect();
+    const noBtnRect = noBtn.getBoundingClientRect( );
+    const maxX = window.innerWidth- noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+    const randomX = Math.floor(Math.random( ) * maxX);
+    const randomY = Math.floor(Math.random( ) * maxY);
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+ });
+
+ 
